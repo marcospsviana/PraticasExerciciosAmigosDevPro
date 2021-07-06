9e5aef6ee5320c50c5288496cb4274a3e07b1c9d
wfoschiera <wfoschiera@gmail.com>
2021-07-05 20:49:53 -0400

    criado exercício sobre pantograma

---
+++ ex.1-pantograma.py
@@ -0,0 +1,12 @@
+""" O Pantograma é uma sentença que contem todas as letras do alfabeto, ao menos um vez. Por exemplo, a sentença: Quem traz CD, LP, fax, engov e whisky JB? (29 letras). 
+Desafio: Dada uma string, detecte se ela é ou não um Pantograma retornando True ou False. Ignore números, pontuações e acentos.
+
+Pantogramas para testes:
+Jane quer LP, fax, CD, giz, TV e bom whisky.[2] (30 letras)
+TV faz quengo explodir com whisky JB. (30 letras)
+Vejo xá gritando que fez show sem playback. (35 letras)
+Todo pajé vulgar faz boquinha sexy com kiwi. (36 letras)
+Vi que ex-janota gordo fez show com playback. (36 letras)
+Já fiz vinho com toque de kiwi para belga sexy. (37 letras)
+Bancos fúteis pagavam-lhe queijo, whisky e xadrez. (41 letras)
+"""
